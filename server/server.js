5a22318b28cd6d8c717685e74e3e648398d76549
kavya <kurmalakavya123@gmail.com>
2026-04-13 20:23:46 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -5,12 +5,16 @@ const notesRoutes = require("./routes/notes");
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "*"
+}));
+
 app.use(bodyParser.json());
 
 app.use("/notes", notesRoutes);
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
