e6555f2d9ad7958c8b56a6dcf541278860667e7c
kavya <kurmalakavya123@gmail.com>
2026-04-13 21:03:38 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,21 +1,12 @@
 const express = require("express");
 const cors = require("cors");
-const bodyParser = require("body-parser");
 const notesRoutes = require("./routes/notes");
 
 const app = express();
 
-app.use(cors({
-  origin: "*",
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  allowedHeaders: ["Content-Type"]
-}));
+app.use(cors());
 
-
-
-
-app.use(bodyParser.json());
-
+app.use(express.json()); 
 app.use("/notes", notesRoutes);
 
 const PORT = process.env.PORT || 5000;
